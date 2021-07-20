b7ea838dfb4ab1bc6383848e877317cb4f0a7f65
Halamai <halamaipawlo@gmail.com>
2021-07-20 22:15:44 +0300

    wwww

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector('.menu-open'),
+    openMenuBtn: document.querySelector('#menu-open'),
     closeMenuBtn: document.querySelector('.btn-close'),
    menu: document.querySelector('.mob-menu'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
-
   function toggleModal() {
+    document.body.classList.toggle('hidden')
     refs.menu.classList.toggle('is-hidden');
   }
 })();
